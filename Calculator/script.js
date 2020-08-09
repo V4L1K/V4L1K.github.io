1d6b9c1f68488788bc062280b37084c4da1a2327
Valik <fredik874@gmail.com>
2020-08-09 16:37:18 +0300

    add site Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,22 @@
+let inpt = document.getElementById('js-inpt');
+let btns = document.getElementById('js-btns');
+
+function Calculate(value){
+	console.log("value:",value);
+	return eval(value);
+}
+function Clear(){inpt.value='';}
+btns.addEventListener('click',function(e){
+	let el = event.target;
+	if(el.id=="js-btns"){
+		return;
+	}
+	let elValue=el.innerText;
+	if(elValue=="="){
+		if(Calculate(inpt.value)==undefined){return}
+		inpt.value=Calculate(inpt.value);
+		return
+	}
+	inpt.value+=elValue;
+	}
+);
